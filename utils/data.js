61a3e409e9dbfc25c93ff479a74759cfeb1508a5
Azura <saamueel64@outlook.es>
2023-08-12 23:20:01 -0600

    Created a data.js file with test users and thoughts.

---
+++ utils/data.js
@@ -0,0 +1,141 @@
+const users = [
+
+    {
+
+        username: 'Johnny Chavez',
+        email: 'johnny.chavez@example.com'
+
+    },
+    {
+
+        username: 'Daryl Mendoza',
+        email: 'daryl.mendoza@example.com'
+
+    },
+    {
+
+        username: 'Alfredo Stevens',
+        email: 'alfredo.stevens@example.com'
+
+    },
+    {
+
+        username: 'David Olson',
+        email: 'david.olson@example.com'
+
+    },
+    {
+
+        username: 'Christy Oliver',
+        email: 'christy.oliver@example.com'
+
+    },
+    {
+
+        username: 'Lesa Simpson',
+        email: 'lesa.simpson@example.com'
+
+    },
+    {
+
+        username: 'Joel Morales',
+        email: 'joel.morales@example.com'
+
+    },
+    {
+
+        username: 'Vicki Rodriguez',
+        email: 'vicki.rodriguez@example.com'
+
+    },
+    {
+
+        username: 'Bradley Garza',
+        email: 'bradley.garza@example.com'
+
+    },
+    {
+
+        username: 'Fred Carr',
+        email: 'fred.carr@example.com'
+
+    }
+
+];
+
+const thoughts = [
+
+    {
+
+        thoughtText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        createdAt: '07/01/2023 18:32:19',
+        username: 'Johnny Chavez'
+
+    },
+    {
+
+        thoughtText: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        createdAt: '07/01/2023 19:23:22',
+        username: 'Daryl Mendoza'
+
+    },
+    {
+
+        thoughtText: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        createdAt: '07/01/2023 20:43:12',
+        username: 'Alfredo Stevens'
+
+    },
+    {
+
+        thoughtText: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        createdAt: '07/01/2023 21:54:00',
+        username: 'David Olson'
+
+    },
+    {
+
+        thoughtText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        createdAt: '07/01/2023 22:32:19',
+        username: 'Christy Oliver'
+
+    },
+    {
+
+        thoughtText: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        createdAt: '07/01/2023 23:23:22',
+        username: 'Lesa Simpson'
+
+    },
+    {
+
+        thoughtText: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        createdAt: '07/02/2023 00:43:12',
+        username: 'Joel Morales'
+
+    },
+    {
+
+        thoughtText: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        createdAt: '07/02/2023 01:54:00',
+        username: 'Vicki Rodriguez'
+
+    },
+    {
+
+        thoughtText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        createdAt: '07/02/2023 02:32:19',
+        username: 'Bradley Garza'
+
+    },
+    {
+
+        thoughtText: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        createdAt: '07/02/2023 03:23:22',
+        username: 'Fred Carr'
+
+    }
+
+];
+
+module.exports = { users, thoughts };
