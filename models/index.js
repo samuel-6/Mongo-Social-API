6c74d4e8ecc06370eb6df5a07aec8652e94fd7dd
Azura <saamueel64@outlook.es>
2023-08-12 23:06:26 -0600

    Created index.js inside models folder to export the User and Thought models.

---
+++ models/index.js
@@ -0,0 +1,6 @@
+// Imports the user and thought models.
+const User = require("./User");
+const Thought = require("./Thought");
+
+// Exports the user and thought models.
+module.exports = {User, Thought};
